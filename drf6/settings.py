20fd40f142603f43c2269ea020d5e6193c93c8c9
paul <ppolanski@free.org.pl>
2024-04-28 10:55:44 +0200

    modified settings.py with hosts open to every IP

--- drf6/settings.py
+++ drf6/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-j(l99xmfld8^#6$zb#u7cqz93)4ui0fnoau5wyn)squuj#rx5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
